d7748cf54212185be59f7d1541bb39d8b219d21e
ERMKS3321 <1044437639@qq.com>
2026-07-15 14:18:42 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.Autofill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,8 +26,8 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
-
     @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) values(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @Autofill(OperationType.INSERT)
     void insert(Employee employee);
 
     /**
@@ -41,6 +43,7 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
+    @Autofill(OperationType.UPDATE)
     void update(Employee employee);
 
     /**
