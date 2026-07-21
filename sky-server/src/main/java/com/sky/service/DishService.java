53e49f63df7e4def0161d4ac91288bd6dcd80269
ERMK5 <1044437639@qq.com>
2026-07-21 18:15:59 +0800

    店铺营业状态设置业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -51,5 +51,13 @@ public interface DishService {
      * @return
      */
     List<Dish> list(Long categoryId);
+
+    /**
+     * 菜品起售停售
+     *
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
 
